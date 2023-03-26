9b85b1a2dd02ce97f7858c944c2576701ea8a964
Damian Potoczny <it.potoczny.damian@gmail.com>
2023-03-26 13:56:49 +0200

    Inicjalizacja

---
+++ zadanie1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+/*Języki programowania Grupa 2DZI Damian Potoczny moduł 1 Funkcje Zadanie 1*/
+
+void reset(int &zmienna)
+{
+    zmienna = 0;
+}
+
+int main()
+{
+    cout << "Jezyki Porgramowania " << "2DZI " << "Damian" << " " << "Potoczny" << endl;
+    cout << "Modul 2 "<< "Funkcje " << "Zadanie 1"<< endl;
+
+
+    return 0;
+}
